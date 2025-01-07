79b38b68a3b9641c4d88f542f91c5400feda69b7
Andrés García <dev@tech-andgar.me>
2025-01-07 18:21:30 +0000

    Publish to gh-pages e3583a348770b788b55e8c8d069af598ced12b38

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "assets/AssetManifest.bin": "5bf8f6f101f44ea381de0b0efa021a97",
-"assets/NOTICES": "f344bc482c54300a1c4cd335fe816098",
+"assets/NOTICES": "f5a7c851115bf5d921a640b0613e0d50",
 "assets/AssetManifest.json": "cb67f5f97d1e4cfc4f6e74e5059dd3c3",
 "assets/fonts/Futura%2520Heavy%2520font.ttf": "3ea9695ee3af8a5e17413f22249f361a",
 "assets/fonts/Futura%2520Extra%2520Black%2520font.ttf": "eefc0fd25f76a11882e20b547c77291c",
@@ -48,8 +48,8 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "/": "07c695d42694cc859c98d7d8bf189918",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "320eaf129778a4158c8016d9d61087ec",
-"main.dart.js": "ad425ed5615ce54907c9627600abc907"};
+"flutter_bootstrap.js": "16768fb405790a8452e22e74282e1754",
+"main.dart.js": "ea49212defa0fb4d0be8da8ec832ea3f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
