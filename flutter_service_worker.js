9208dfffe34030bae97e00b13772e00dc9451be1
Andrés García <dev@tech-andgar.me>
2025-01-10 04:48:11 +0000

    Publish to gh-pages 9139d7b12b8477ff8951a6aa980eb0d93f2759cb

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,8 +48,8 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "/": "07c695d42694cc859c98d7d8bf189918",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "4cfe2a87b5dde821e369d20978e3e63c",
-"main.dart.js": "e8596fdd14cb3961eafb6b28f3867109"};
+"flutter_bootstrap.js": "f00d0f7cf40343785ea8e09c6291110f",
+"main.dart.js": "f639ccfb4b4a8a24999783f2c669fc2b"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
