b2f7688f64ca92a81adf99f5cf9b4f7d73ece9cd
Andrés García <dev@tech-andgar.me>
2025-01-09 23:42:19 -0500

    fix: improvement UI Dialog in AnamnesisStep2Screen

--- lib/modules/anamnesis/presentation/anamnesis_step2_screen.dart
+++ lib/modules/anamnesis/presentation/anamnesis_step2_screen.dart
@@ -74,32 +74,131 @@ class AnamnesisStep2Screen extends ConsumerWidget {
     await showDialog<void>(
       context: context,
       builder: (BuildContext dialogContext) {
-        return AlertDialog(
-          backgroundColor: Colors.black87,
-          title: const CustomText('Cuestionario finalizado', fontSize: 20),
-          content: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('Desarrollado por Andres Garcia (TECH-ANDGAR)'),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Text('Hecho por Flutter'),
-                  FlutterLogo(size: 24),
-                ],
+        return Consumer(
+          builder: (context, ref, _) {
+            final anamnesisForm1Data = ref.watch(anamnesisForm1ViewModel);
+            final anamnesisForm2Data = ref.watch(anamnesisForm2ViewModel);
+
+            return AlertDialog(
+              backgroundColor: Colors.black87,
+              title: const CustomText(
+                'Cuestionario finalizado',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
-          actions: <Widget>[
-            CustomButton(
-              label: 'Ir a la pantalla de inicio',
-              onTap: () {
-                Navigator.of(dialogContext).pop();
-                router.pushReplacement(RoutesName.anamnesisStep1);
-              },
-              inDialog: true,
-            ),
-          ],
+              content: SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.5,
+                width: double.maxFinite,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: <Widget>[
+                          const CustomText(
+                            '¡Gracias por completar el cuestionario!',
+                          ),
+                          const SizedBox(height: 16),
+                          const CustomText(
+                            'Resumen de respuestas:',
+                            fontWeight: FontWeight.bold,
+                          ),
+                          const SizedBox(height: 8),
+                          ListTile(
+                            title: const CustomText(
+                              '- ¿Ha tenido operaciones? ¿Cuáles y hace cuanto tiempo?',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            subtitle: CustomText(
+                              anamnesisForm1Data.operation,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          ListTile(
+                            title: const CustomText(
+                              '- ¿Tiene o tuvo alguna enfermedad diagnosticada o tratada por un médico?',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            subtitle: CustomText(
+                              anamnesisForm1Data.disease,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          ListTile(
+                            title: const CustomText(
+                              '- ¿Tiene dolores frecuentes y no ha consultado al médico?',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            subtitle: CustomText(
+                              anamnesisForm2Data.painFrequent ?? false
+                                  ? 'Si, tiene dolores frecuentes'
+                                  : 'Sin dolores frecuentes',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          ListTile(
+                            title: const CustomText(
+                              '- ¿Le ha dicho al médico que tiene algún problema en los huesos o en las articulaciones, que pueda desfavorecer con el ejercicio?',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w300,
+                            ),
+                            subtitle: CustomText(
+                              anamnesisForm2Data.problemBonesJoints ?? false
+                                  ? 'Problema en huesos o articulaciones'
+                                  : 'Sin problemas en huesos o articulaciones',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const Text(
+                            'Desarrollado por Andres Garcia \n(TECH-ANDGAR)',
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Semantics(
+                                label: 'Hecho por Flutter',
+                                child: const ExcludeSemantics(
+                                  child: Row(
+                                    children: [
+                                      Text('Hecho por '),
+                                      FlutterLogo(size: 24),
+                                      Text(' Flutter'),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                CustomButton(
+                  label: 'Ir a la pantalla de inicio',
+                  onTap: () {
+                    Navigator.of(dialogContext).pop();
+                    router.pushReplacement(RoutesName.anamnesisStep1);
+                  },
+                  inDialog: true,
+                ),
+              ],
+            );
+          },
         );
       },
     );
