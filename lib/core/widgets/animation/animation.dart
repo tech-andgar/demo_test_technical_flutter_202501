13e2c2f2e14c4111979dbddbfab9c6d95adb7256
Andrés García <dev@tech-andgar.me>
2025-01-11 14:39:09 -0500

    feat: add widgets animations

---
+++ lib/core/widgets/animation/animation.dart
@@ -0,0 +1,3 @@
+export 'animated_alert_dialog_accessability_widget.dart';
+export 'animated_fade_up_visibility.dart';
+export 'animated_screen.dart';
--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -1,5 +1,6 @@
 //  Created in Dart by Andres Garcia (TECH-ANDGAR) on 2025-01-03.
 
+export 'animation/animation.dart';
 export 'atom/atom.dart';
 export 'constants.dart';
 export 'molecule/molecule.dart';
