7ec76d7fa7f4a7718434569db26c5aaff11a88ed
Andrés García <dev@tech-andgar.me>
2025-01-11 14:40:49 -0500

    refactor: createAnimation method from extension

--- lib/modules/anamnesis/presentation/anamnesis_steps_screen_animator.dart
+++ lib/modules/anamnesis/presentation/anamnesis_steps_screen_animator.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/widgets.dart';
 
+import '../../../core/extensions/extensions.dart';
+
 class AnamnesisStepsScreenAnimator {
   AnamnesisStepsScreenAnimator(this.controller)
-      : title = _createAnimation(controller, 0.0, 0.2),
-        subtitle = _createAnimation(controller, 0.15, 0.35),
-        question1Label = _createAnimation(controller, 0.3, 0.5),
-        question1TextField = _createAnimation(controller, 0.45, 0.65),
-        question2Label = _createAnimation(controller, 0.6, 0.8),
-        question2TextField = _createAnimation(controller, 0.75, 0.95),
-        button =
-            _createAnimation(controller, 0.85, 1.0, curve: Curves.easeInOut);
-
-  static Animation<double> _createAnimation(
-    AnimationController controller,
-    double begin,
-    double end, {
-    Curve curve = Curves.easeOut,
-  }) {
-    return CurvedAnimation(
-      parent: controller,
-      curve: Interval(begin, end, curve: curve),
-    );
-  }
+      : title = controller.createAnimation(0.0, 0.2),
+        subtitle = controller.createAnimation(0.15, 0.35),
+        question1Label = controller.createAnimation(0.3, 0.5),
+        question1TextField = controller.createAnimation(0.45, 0.65),
+        question2Label = controller.createAnimation(0.6, 0.8),
+        question2TextField = controller.createAnimation(0.75, 0.95),
+        button = controller.createAnimation(0.85, 1.0, curve: Curves.easeInOut);
 
   final Animation<double> title;
   final Animation<double> subtitle;
